282229a857bf70cc23940b8b8b5aae754742c971
monishstark <monishkumarms3@gmail.com>
2022-02-05 02:01:30 +0530

    update

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./CheckoutProduct.css";
 
-function CheckoutProduct({ title, image, price }) {
+function CheckoutProduct({ title, image, price, disc }) {
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct_image" src={image} />
       <div className="checkoutProduct_info">
         <p className="checkoutProduct_title">{title}</p>
+        <p className="checkoutProduct_disc">{disc}</p>
         <p className="checkoutProduct_price">
           <small>₹</small>
           <strong>{price}</strong>
